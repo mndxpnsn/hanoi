d918660745207948cf73fe92646fa0365fd9e41b
derek <harrison.derek.w@gmail.com>
2022-05-28 22:42:22 +0200

    initial commit tower of hanoi

---
+++ src/Hanoi.java
@@ -0,0 +1,36 @@
+public class Hanoi {
+
+    static void move(int d, String f, String t) {
+        System.out.println("move disc " + d + " from " + f + " to " + t);
+    }
+
+    static int han(int n, String a, String b, String c) {
+        // Move n disks from a to c via b
+        int res = 0;
+
+        if(n == 0)
+            return 0;
+
+        if(n > 0) {
+            res += han(n - 1, a, c, b);
+            move(n, a, c);
+            res += han(n - 1, b, a, c);
+        }
+
+        res++;
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        // Number of disks
+        int n = 4;
+
+        // Compute solution and number of moves
+        int num_moves = han(n, "A", "B", "C");
+
+        // Print results
+        System.out.println("total number of moves: " + num_moves);
+    }
+}
